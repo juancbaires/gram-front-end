0aa8e2433b283ec820787c399700604dae7a05ef
Juan Baires <>
2020-02-11 19:23:41 -0500

    added a signup method. Still need to test it

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,26 +5,69 @@ import Splash from "./splash";
 import { Switch, Route } from "react-router";
 import Register from "./assets/register";
 import decode from "jwt-decode";
+import axios from "axios";
 export default class App extends Component {
   state = {
     isLoggedIn: false,
-    user: ""
+    user: "",
+    error: ""
   };
+
+  componentDidMount() {
+    this.setuser();
+  }
   setuser = () => {
     if (localStorage.token) {
       this.setState({ isLoggedIn: true, user: decode(localStorage.token) });
     }
   };
 
-  handleLoggedIn = oldUser => {};
+  handleAuth = async oldUser => {
+    try {
+      const userData = await axios.post("http://localhost:3000/users/login", {
+        ...oldUser
+      });
+      const { data } = userData;
+      localStorage.token = data.token;
+      this.setState({ isLoggedIn: true });
+      this.props.history.push("/");
+    } catch (error) {
+      this.setState({ isLoggedIn: false, error });
+    }
+  };
+
+  createUser = async newUser => {
+    try {
+      const userData = await axios.post("http://localhost:3000/users/signup", {
+        ...newUser
+      });
+      const { data } = userData;
+      localStorage.token = data.token;
+      this.setState({ isLoggedIn: true });
+      this.props.history.push("/");
+    } catch (error) {
+      this.setState({ error });
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <Header></Header>
-        <Switch>
-          <Route path="/register" component={Register}></Route>
-          <Route path="/" component={Splash}></Route>
-        </Switch>
+        <main>
+          <Switch>
+            <Route
+              path="/register"
+              render={() => (
+                <Register
+                  {...this.state}
+                  handleAuth={this.createUser}
+                ></Register>
+              )}
+            ></Route>
+            <Route path="/" component={Splash}></Route>
+          </Switch>
+        </main>
       </div>
     );
   }
--- front-end/src/assets/register.js
+++ front-end/src/assets/register.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
 import "./register.css";
+
 export default class register extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.createUser(this.state);
+  };
+
   render() {
     return (
       <div className="wrapper">
@@ -20,20 +35,30 @@ export default class register extends Component {
             </span>
             <span className="form_blocks">
               <i className="material-icons icons">alternate_email</i>
-              <input type="text" placeholder="Email" />
+              <input
+                onChange={this.handleChange}
+                type="text"
+                placeholder="Username"
+                name="username"
+              />
             </span>
             <span className="form_blocks">
               <i className="material-icons icons">visibility_off</i>
-              <input type="password" placeholder="Password" />
+              <input
+                onChange={this.handleChange}
+                type="password"
+                placeholder="Password"
+                name="password"
+              />
             </span>
-            <span className="form_blocks">
+            {/* <span className="form_blocks">
               <i className="material-icons icons">person</i>
               <input type="text" placeholder="Your Name" />
-            </span>
-            <span className="form_blocks">
+            </span> */}
+            {/* <span className="form_blocks">
               <i className="material-icons icons">face</i>
               <input type="number" placeholder="Age" min="0" max="100" />
-            </span>
+            </span> */}
             {/* this below is the button */}
             <span className="form_blocks">
               <input type="submit" value="SIGN UP" />
